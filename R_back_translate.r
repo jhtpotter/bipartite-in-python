c434e8c7519a7b97b46e438bf6f53d0c0b38cd26
Josh Potter <jhtpotter@gmail.com>
2016-02-24 13:43:38 +0000

    updated

--- R_back_translate.r
+++ R_back_translate.r
@@ -3,13 +3,16 @@
 
 eval_df <- function(df, taxadict){
   predators = colnames(df)
+  print("predators:", predators)
   preyOTUs = rownames(df)
+  print("prey OTUs:", preyOTUs)
   for (i in 1:ncol(df)){
     colvalues <- numeric()
     predator <- predators[i]
     preygained <- 0
     preylost <- 0
-    if (! predator %in% names(taxadict)){
+    if (!(predator %in% names(taxadict))){
+      # INSERT DIE STATEMENT
       next()
     }
     preybreadth <- length(taxadict$predator) - 1
@@ -24,6 +27,7 @@ eval_df <- function(df, taxadict){
         } else if (length(which(taxadict$predator==preyOTU)) == 1){
           next()
         } else {
+          # INSERT DIE STATEMENT
           next()
         }
       } else if (interactionval == 0){
@@ -34,17 +38,21 @@ eval_df <- function(df, taxadict){
           taxadict$predator <- taxadict$predator[taxadict$predator != preyOTU]
           preylost <- preylost + 1
         } else {
+          # INSERT DIE STATEMENT
           next()
         }
       } else {
+        # INSERT DIE STATEMENT
         next()
       }
     }
     colvalsum <- sum(colvalues)
     if (colvalsum != length(taxadict$predator) - 1){
+      # INSERT DIE STATEMENT
       next()
     }
     if (colvalsum != preybreadth + preygained - preylost){
+      # INSERT DIE STATEMENT
       next()
     }
     taxadict$predator[1] = colvalsum
@@ -66,3 +74,61 @@ empty <- function(df, count=FALSE){
     row = df[i,]
   }
 }
+
+extinction <- function(df, participant, method, interactionDict, switchDict, extOTUdict){
+  numcols = ncol(df)
+  numrows = nrow(df)
+  rowsumVec = rowSums(df)
+  colsumVec = colSums(df)
+  if (participant == "both" && method == "random"){
+    # Randomly pick a participant:
+    partIdx = sample(1:2, 1)
+    participant = participants[partIdx]
+    print("Participant is now:", participant)
+  }
+  if (method == "random"){
+    rowextin = sample(1:numrows)
+    colextin = sample(1:numcols)
+    if (participant == "lower"){
+      for (i in 1:numcols){
+        df[rowextin,i] = 0
+      }
+    }
+    if (participant == "higher"){
+      for (j in 1:numrows){
+        df[j,colextin] = 0
+      }
+    }
+  }
+  else if (method == "abundance"){
+    rowsumVec = numeric()
+    colsumVec = numeric()
+    df = df[sample(1:nrow(df),nrow(df)),]
+    df = df[,sample(1:ncol(df),ncol(df))]
+    rowsumVec = rowSums(df)
+    colsumVec = colSums(df)
+    minCol = which(colsumVec == min(colsumVec))
+    minRow = which(rowsumVec == min(rowsumVec))
+    print(minCol,minRow)
+    if (participant == "lower"){
+      extOTU = rownames(df[minRow,])
+      if (extOTU %in% names(extOTUdict)){
+        # INSERT DIE STATEMENT
+        next()
+      }
+      else {
+        extOTUdict <- c(extOTUdict, extOTU=1)
+      }
+      for (i in 1:ncol(df)){
+        currentPredator = colnames(df[,i])
+        nonpreyidxs = which(df[,i]==0 && !(rownames(df) %in% extOTUdict))
+        print(nonpreyidxs)
+        if (switchDict$currentPredator == "swticher" && length(nonpreyidxs) > 0){
+          newprey = nonpreyidxs[sample(1:length(nonpreyidxs),1)]
+        }
+      }
+    }
+  }
+  
+}
+
